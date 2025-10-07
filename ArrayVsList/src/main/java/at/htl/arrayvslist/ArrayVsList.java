3a910ee17344a63f1644156f24bc71173ea5a328
Adrian Fritz <adrian.fritz@it.htl-hl.ac.at>
2025-10-07 11:09:47 +0200

    added comment

--- ArrayVsList/src/main/java/at/htl/arrayvslist/ArrayVsList.java
+++ ArrayVsList/src/main/java/at/htl/arrayvslist/ArrayVsList.java
@@ -1,5 +1,22 @@
 package at.htl.arrayvslist;
 
+/**
+ * Dieses Beispiel zeigt die Unterschiede zwischen einem Array und einer
+ * Liste
+ *
+ * Das Arbeiten mit Listen bietet dem Programmierer viele Erleichterungen
+ * gegenüber dem Arbeiten direkt mit Arrays.
+ *
+ * z.B.:
+ * einfaches Hinzufügen und Entfernen von Datensätzen
+ * einfacher Zugriff auf Elemente (nicht nur über Index)
+ * Elementsuche
+ * u.v.m.
+ *
+ */
+
 public class ArrayVsList {
+
+    // Fields *************************************************************************************************************
     
 }
