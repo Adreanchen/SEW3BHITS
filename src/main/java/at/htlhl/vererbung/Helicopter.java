50035e0d11229d25729fc52bd1810e793f796a68
Adrian Fritz <adrian.fritz@it.htl-hl.ac.at>
2025-10-07 10:19:43 +0200

    /

---
+++ src/main/java/at/htlhl/vererbung/Helicopter.java
@@ -0,0 +1,21 @@
+package at.htlhl.vererbung;
+
+public class Helicopter extends Aircraft {
+    private float rotorBladeSize;
+
+    public Helicopter() {
+        this.rotorBladeSize = 8.3f;
+    }
+
+    public void takeoff() {
+        System.out.println("An Helicopter is taking off.  ");
+    }
+
+    public void fly() {
+        System.out.println("An Helicopter is flying.  ");
+    }
+
+    public void land() {
+        System.out.println("An Helicopter is landing.  ");
+    }
+}
